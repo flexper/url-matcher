eef916c77a8fbf53cbd49a104434e4c163731e81
lucas137 <lucas@flexper.fr>
2022-06-01 16:45:00 +0200

    feat(url-matcher): Add url-matcher code and unit tests

---
+++ src/index.ts
@@ -0,0 +1,40 @@
+import extractDomain from "extract-domain";
+
+function cleanDomain(domain: string) {
+  const cleaned = domain
+      ?.replace('https://', '')
+      ?.replace('http://', '')
+      ?.replace('www.', '')
+      ?.replace(/\/+$/, '');
+  // Handle special domain with /en for example
+  // We want to remove all slashes from web url (ex: www.clustree.com/en)
+  if (cleaned && cleaned.indexOf('/') > 1) {
+    return cleaned.substring(0, cleaned.indexOf('/'));
+  }
+  return cleaned;
+}
+
+export const urlMatcher = (domainA: string, domainB: string): boolean => {
+  if (!domainA || !domainB) {
+    return false;
+  }
+  // Easy case
+  const cleanDomainA = cleanDomain(domainA);
+  const cleanDomainB = cleanDomain(domainB);
+
+  if (cleanDomainA === cleanDomainB) {
+    return true;
+  }
+  // Sub domain, and more complicated cases
+  // However. This package will also remove the sub domain,
+  // and unless the TLD option is used, anything other than the domain
+  // and second level domain (so www.example.co.uk would return co.uk)
+  // So we use the tld option to avoid this.
+  const parseResultA = extractDomain(domainA, { tld: true });
+  const parseResultB = extractDomain(domainB, { tld: true });
+  return (
+      parseResultA === parseResultB ||
+      parseResultA === domainB ||
+      domainA === parseResultB
+  );
+};
---
+++ test/url-matcher.test.ts
@@ -0,0 +1,75 @@
+import { urlMatcher } from "../src";
+
+describe('Url Matcher', () => {
+  it('should match 1', () => {
+    const toMatch = 'ibm.com';
+    const domain = 'ibm.com';
+    expect(urlMatcher(domain, toMatch)).toBeTruthy();
+  });
+
+  it('should match 2', () => {
+    const toMatch = 'https://ibm.com';
+    const domain = 'https://ibm.com';
+    expect(urlMatcher(domain, toMatch)).toBeTruthy();
+  });
+
+  it('should match 3', () => {
+    const toMatch = 'ibm.com';
+    const domain = 'www.ibm.com';
+    expect(urlMatcher(domain, toMatch)).toBeTruthy();
+  });
+
+  it('should match 4', () => {
+    const toMatch = 'www.fetcher.ai';
+    const domain = 'fetcher.ai';
+    expect(urlMatcher(domain, toMatch)).toBeTruthy();
+  });
+
+  it('should match 5', () => {
+    const toMatch = 'www.do.swile.co';
+    const domain = 'https://swile.co';
+    expect(urlMatcher(domain, toMatch)).toBeTruthy();
+  });
+
+  it('should match 6', () => {
+    const toMatch = 'www.do.swile.co/LK';
+    const domain = 'https://swile.co';
+    expect(urlMatcher(domain, toMatch)).toBeTruthy();
+  });
+
+  it('should match 7', () => {
+    const toMatch = 'www.do.swile.co/LK';
+    const domain = 'swile.co';
+    expect(urlMatcher(domain, toMatch)).toBeTruthy();
+  });
+
+  it('should match 8', () => {
+    const toMatch = 'ibm.com';
+    const domain = 'ibm.fr';
+    expect(urlMatcher(domain, toMatch)).toBeTruthy();
+  });
+
+  it('should not match 9', () => {
+    const toMatch = 'www.swile.co';
+    const domain = 'https://swile.co';
+    expect(urlMatcher(domain, toMatch)).toBeTruthy();
+  });
+
+  it('should not match 1', () => {
+    const toMatch = 'www.do.swile.co/LK';
+    const domain = 'https://swyle.co';
+    expect(urlMatcher(domain, toMatch)).toBeFalsy();
+  });
+
+  it('should not match 2', () => {
+    const toMatch = 'www.flexper.fr';
+    const domain = 'equitylabs.fr';
+    expect(urlMatcher(domain, toMatch)).toBeFalsy();
+  });
+
+  it('should not match 3', () => {
+    const toMatch = 'www.flexper.fr';
+    const domain = 'flexper-en.fr';
+    expect(urlMatcher(domain, toMatch)).toBeFalsy();
+  });
+});
